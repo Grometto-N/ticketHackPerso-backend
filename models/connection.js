5c1fd1ebabf9be4af3e4f95d94024a099cd33cce
GROMETTO N <nicolas.grometto@laposte.net>
2023-01-30 15:41:40 +0100

    problemen .env in deployement solved

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const connectionString = process.env.CONNECTION_STRING;
 
-console.log(process.env.CONNECTION_STRING)
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
