1d865d8557fe7dafd65304413e51f0efe5f323f9
GROMETTO N <nicolas.grometto@laposte.net>
2023-01-30 15:37:24 +0100

    pb connection with .env

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const connectionString = process.env.CONNECTION_STRING;
 
+console.log(process.env.CONNECTION_STRING)
+
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
